f89c6aee71fcb6b90be7f029152ea0ee8bbed6db
Mustafa Uysal <mustafauyysl@gmail.com>
2021-02-17 12:27:17 -0800

    Initial Commit

---
+++ AnimalWorld/AnimalWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalWorldApp.swift
+//  AnimalWorld
+//
+//  Created by Mustafa on 17.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimalWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
